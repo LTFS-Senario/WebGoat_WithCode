b3b1759f74f9f3380e0e3a737be271c8c36f3e2e
thaadur <lokesh.tk@cyberpwn.com>
2025-08-28 16:29:40 +0530

    Update Snyk_Code_Scan.sh

--- Snyk_Code_Scan.sh
+++ Snyk_Code_Scan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Trigger Snyk Code scan via import API
-curl -X POST "https://api.snyk.io/v1/org/0b77e63a-6113-423c-900f-fc8c64feb95f/integrations/497609cd-3813-42fc-94b0-6a6089b6c801/impo>-H "Authorization: token a3580c50-a2ed-4f00-891b-604ad8e3007c" \
+curl -X POST "https://api.snyk.io/v1/org/0b77e63a-6113-423c-900f-fc8c64feb95f/integrations/497609cd-3813-42fc-94b0-6a6089b6c801/import" \
+-H "Authorization: token a3580c50-a2ed-4f00-891b-604ad8e3007c" \
 -H "Content-Type: application/json" \
 -d '{
   "target": {
